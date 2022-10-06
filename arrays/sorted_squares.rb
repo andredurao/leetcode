d7da1965c590fe75ad2438515ec8246f07d45081
André Durão <andre.durao@wovn.io>
2022-10-06 23:24:01 +0900

    O(n) improvements in sorted squares from explanations

--- arrays/sorted_squares.rb
+++ arrays/sorted_squares.rb
@@ -1,43 +1,21 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sorted_squares(nums)
-  # populate square arrays
-  # O(n) time, O(n) space
-  positive_squares = []
-  negative_squares = []
-  nums.each do |num|
-    square = num * num
-    if num < 0
-      negative_squares << square
-    else
-      positive_squares << square
-    end
-  end
-
-  # sorting by bubbling
-  # O(n) time, O(n) space
+  l = 0
+  r = nums.size - 1
   result = []
-  while positive_squares.any? || negative_squares.any?
-    # inserting and ordering cases
-    if positive_squares.any? && negative_squares.any?
-      if positive_squares.first <= negative_squares.last
-        result << positive_squares.shift
-      else
-        result << negative_squares.pop
-      end
-    end
 
-    #draining empty queue cases O(1)
-    if negative_squares.empty?
-      result += positive_squares
-      positive_squares = []
-    end
-    if positive_squares.empty?
-      result += negative_squares.reverse
-      negative_squares = []
+  while l != r
+    if nums[l].abs >= nums[r]
+      result.unshift(nums[l] ** 2)
+      l += 1
+    else
+      result.unshift(nums[r] ** 2)
+      r -= 1
     end
   end
-  result
+
+  result.unshift(nums[r] ** 2)
 end
 
 nums = [-4,-1,0,3,10]
