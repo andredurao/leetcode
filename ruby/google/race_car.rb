b0bf26ea6e71ddd63dee2582f8ac0a694e4eb7a9
André Durão <andre.durao@wovn.io>
2022-10-23 11:33:17 +0900

    WIP: race car translation from python's solution TIMEOUT

--- ruby/google/race_car.rb
+++ ruby/google/race_car.rb
@@ -1,44 +1,49 @@
 # @param {Integer} target
 # @return {Integer}
 def racecar(target)
-  a_commands = []
-  move_helper(target, a_commands, 0, 1, 'A', 0)
-  r_commands = []
-  move_helper(target, r_commands, 0, 1, 'R', 0)
-  p a_commands
-  p r_commands
-end
+  limit = target.bit_length + 1
+  barrier = 1 << limit
+  pq = [[0, target]]
+  dist = [Float::INFINITY] * (2 * barrier + 1)
+  dist[target] = 0
+  # binding.irb
 
-def move_helper(target, commands=[], pos=0, speed=1, sequence='', previous_pos=0)
-  p [target, pos, speed, sequence, previous_pos] ; sleep 0.3
-  new_pos = pos
-  if pos == target
-    commands << sequence
-    return
-  else
-    direction = (target - previous_pos) - (target - pos)
-    if direction < 0
-      p 'wrong way'
-      return
-    else
-      p 'right way'
-      if target >= 0
-        return if pos > target
-      else
-        return if pos < target
+  while pq.any?
+    steps, targ = pq.pop
+    next if targ && dist[targ] && dist[targ] > steps
+
+    0.upto(limit+1) do |k|
+      walk = (1 << k) - 1
+      steps2 = steps + k + 1
+      targ2 = walk - targ
+      steps2 -= 1 if walk == targ  # No "R" command if already exact
+
+      if targ2.abs <= barrier && steps2 < dist[targ2]
+        heap_push(pq, [steps2, targ2])
+        dist[targ2] = steps2
       end
     end
   end
-  command = sequence[-1]
-  if command == 'A'
-    new_pos += speed
-    speed *= 2
-  elsif command == 'R'
-    speed = speed > 0 ? -1 : 1
-  end
-  move_helper(target, commands, new_pos, speed, sequence+'A', pos)
-  move_helper(target, commands, new_pos, speed, sequence+'R', pos)
+
+  return dist[0]
 end
 
-target = 3
+def heap_push(arr, x)
+  index = arr.bsearch_index { |y| (y[0] <=> x[0]) >= 0 } || arr.size
+  arr.insert(index, x)
+end
+
+target = 330
 p racecar(target)
+
+# # https://leetcode.com/problems/minimize-deviation-in-array/discuss/1053053/Ruby-Working-with-Priority-Queue-in-Ruby-one-approach/842794/
+# # Insert element into ASC sorted array.
+# def heappush(arr, x)
+#   index = arr.bsearch_index { |y| (y <=> x) >= 0 } || arr.size
+#   arr.insert(index, x)
+# end
+# # Remove element from ASC sorted array if the element is present.
+# def heappop(arr, x)
+#   index = arr.bsearch_index { |y| (y <=> x) >= 0 }
+#   arr.delete_at(index) if index && arr[index] == x
+# end
