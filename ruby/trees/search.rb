2ea0ea7d54a87dc3a6bc176fc000757a5fd9d3e4
André Durão <andre.durao@wovn.io>
2022-10-25 22:37:45 +0900

    Adds binary search basic algor

---
+++ ruby/trees/search.rb
@@ -0,0 +1,26 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+  l = 0
+  r = nums.size - 1
+  while l <= r
+    mid = (l+r) / 2
+    return mid if nums[mid] == target
+    if nums[mid] < target
+      l = mid + 1
+    else
+      r = mid - 1
+    end
+  end
+  -1
+end
+
+
+nums = [-1,0,3,5,9,12]
+target = 9
+p search(nums, target)
+
+nums = [-1,0,3,5,9,12]
+target = 2
+p search(nums, target)
