a9ec746d10d360a82bdc2c00977a08c9879ed7c2
Andre Durao <andreluisdurao@gmail.com>
2023-05-14 18:59:44 +0900

    Adds final solution (based on editorial)

--- go/arrays/2038-remove-colored-pieces/main.go
+++ go/arrays/2038-remove-colored-pieces/main.go
@@ -1,7 +1,8 @@
+// https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/description/
+
 package main
 
 import (
-	"container/list"
 	"fmt"
 )
 
@@ -12,61 +13,21 @@ func main() {
 }
 
 func winnerOfGame(colors string) bool {
-	if len(colors) < 3 {
-		return false
-	}
+	alice := 0
+	bob := 0
 
-	linked_list := init_list(colors)
-	fmt.Printf("colors %s\n", colors)
-	for linked_list.Len() >= 3 {
-		fmt.Println("ALICE")
-		if play(linked_list, 'A') {
-			fmt.Println("ALICE removed a letter")
-			debug(linked_list)
-		} else {
-			fmt.Println("BOB WINS")
-			return false
+	for i, _ := range colors {
+		if i == 0 || i == len(colors)-1 {
+			continue
+		}
+		if colors[i-1] == colors[i] && colors[i] == colors[i+1] {
+			if colors[i] == 'A' {
+				alice++
+			} else {
+				bob++
+			}
 		}
 
-		fmt.Println("BOB")
-		if play(linked_list, 'B') {
-			fmt.Println("BOB removed a letter")
-			debug(linked_list)
-		} else {
-			fmt.Println("ALICE WINS")
-			return true
-		}
 	}
-
-	return false
-}
-
-func init_list(colors string) *list.List {
-	l := list.New()
-	for _, char := range colors {
-		l.PushBack(char)
-	}
-	return l
-}
-
-// given a color, return the index if the play was possible, otherwise -1
-func play(list *list.List, color rune) bool {
-	if list.Len() < 3 {
-		return false
-	}
-	for e := list.Front().Next(); e.Next() != nil; e = e.Next() {
-		if color == e.Value && color == e.Prev().Value && color == e.Next().Value {
-			list.Remove(e)
-			return true
-		}
-	}
-	return false
-}
-
-func debug(list *list.List) {
-	for e := list.Front(); e != nil; e = e.Next() {
-		char, _ := e.Value.(rune)
-		fmt.Printf("%s", string(char))
-	}
-	fmt.Printf("\n")
+	return alice-bob >= 1
 }
