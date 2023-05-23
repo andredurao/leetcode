737aad45e46874f8dfbf628b8b3497b63fbee593
Andre Durao <andreluisdurao@gmail.com>
2023-05-23 16:52:12 +0900

    Fix typo

---
+++ go/arrays/908_smallest_range/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/smallest-range-i/
+
+func smallestRangeI(nums []int, k int) int {
+	return 0
+}
+
+func main() {
+	nums := []int{1}
+	result := smallestRangeI(nums, 0)
+	fmt.Println("%v", result)
+}
