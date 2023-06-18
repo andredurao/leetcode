93c03d675c47d9d6250465a622b037495f7375ac
Andre Durao <andreluisdurao@gmail.com>
2023-06-18 12:57:19 +0900

    Problem 15 / part2

--- go/arrays/15_3sum/main.go
+++ go/arrays/15_3sum/main.go
@@ -12,41 +12,32 @@ func main() {
 }
 
 func threeSum(nums []int) [][]int {
-	sumMap := make(map[int](map[string][]int))
-	for i := 0; i < len(nums)-2; i++ {
-		for j := i + 1; j < len(nums)-1; j++ {
-			sum := nums[i] + nums[j]
-			_, found := sumMap[sum]
-			if !found {
-				sumMap[sum] = make(map[string][]int)
-			}
-			pair := []int{nums[i], nums[j]}
-			slices.Sort(pair)
-			strPair := fmt.Sprintf("%d,%d", pair[0], pair[1])
-			pair = append(pair, []int{i, j}...)
-			sumMap[sum][strPair] = pair
-		}
-	}
-
-	resultMap := make(map[string][]int, 0)
-
-	for i, n := range nums {
-		pairs, found := sumMap[-n]
-		if found {
-			for _, values := range pairs {
-				if i != values[2] && i != values[3] {
-					item := []int{n, values[0], values[1]}
-					slices.Sort(item)
-					strItem := fmt.Sprintf("%d,%d,%d", item[0], item[1], item[2])
-					resultMap[strItem] = item
-				}
-			}
-		}
-	}
-
+	slices.Sort(nums)
 	result := make([][]int, 0)
-	for _, item := range resultMap {
-		result = append(result, item)
+	for i := 0; i < len(nums) && nums[i] <= 0; i++ {
+		if i == 0 || nums[i] != nums[i-1] {
+			result = append(result, twoSum(nums, i)...)
+		}
+	}
+	return result
+}
+
+func twoSum(nums []int, i int) [][]int {
+	result := make([][]int, 0)
+
+	seen := make(map[int]struct{})
+	j := i + 1
+	for j < len(nums) {
+		complement := -nums[i] - nums[j]
+		_, found := seen[complement]
+		if found {
+			result = append(result, []int{nums[i], nums[j], complement})
+			for j+1 < len(nums) && nums[j] == nums[j+1] {
+				j++
+			}
+		}
+		seen[nums[j]] = struct{}{}
+		j++
 	}
 	return result
 }
