d7ad586d8c61283f26e681bc71ec299ccbd9d6e3
Andre Durao <andreluisdurao@gmail.com>
2024-06-06 10:35:18 -0300

    Adds min heap data structure pkg

---
+++ go/data_structures/min_heap.go
@@ -0,0 +1,27 @@
+package datastructures
+
+// based on leetcode solution for problem 846
+type MinHeap []int
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+func (h MinHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	heap := *h
+	n := len(heap)
+	x := heap[n-1]
+	*h = heap[0 : n-1]
+
+	return x
+}
