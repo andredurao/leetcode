bf7e8b959c9796dfcbbe961d31d5fa09a80b0a18
Andre Durao <andreluisdurao@gmail.com>
2024-06-15 20:12:22 -0300

    Problem 502, trying to solve with a min heap

--- go/arrays/502_ipo/main.go
+++ go/arrays/502_ipo/main.go
@@ -4,53 +4,54 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	k := 2
+	k := 1
 	w := 0
 	profits := []int{1, 2, 3}
-	capital := []int{0, 1, 1}
+	capital := []int{0, 1, 2}
 	res := findMaximizedCapital(k, w, profits, capital)
 	fmt.Println(res)
 }
 
-func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
-	dp := [][]int{}
+type MinHeap []int
 
-	for i := 0; i <= k+1; i++ {
-		dp = append(dp, make([]int, k+1))
-	}
-
-	for i := range dp {
-		for j := range dp[i] {
-			dp[i][j] = -1
-		}
-	}
-
-	// this looks like a knapsack problem of sorts, because they are looking to maximize the profit
-	// but with a difference: the capacity(k) is fixed, but items can only be used if there is enough capital
-	// https://en.wikipedia.org/wiki/Knapsack_problem
-	total := knapsack(k, w, &profits, &capital, len(profits)-1, &dp)
-
-	return total
+func (h *MinHeap) Push(x int) {
+	*h = append(*h, x)
 }
 
-func knapsack(k int, w int, profits *[]int, capital *[]int, index int, dp *[][]int) int {
-	if index < 0 {
-		return 0
+func (h *MinHeap) Pop() int {
+	heap := *h
+	n := len(heap)
+	x := heap[n-1]
+	*h = heap[0 : n-1]
+
+	return x
+}
+
+func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
+	// based on editorial solution
+	projects := [][]int{}
+	for i := range capital {
+		projects = append(projects, []int{capital[i], profits[i]})
 	}
-	if (*dp)[index][k] != -1 || k == 0 {
-		return (*dp)[index][k]
+	sort.Slice(projects, func(i, j int) bool { return projects[i][0] < projects[j][0] })
+
+	q := MinHeap{}
+	index := 0
+	for i := 0; i < k; i++ {
+		for index < len(capital) && projects[index][0] <= w {
+			// push negated
+			q.Push(projects[index][1] * -1)
+			index++
+		}
+		if len(q) == 0 {
+			break
+		}
+		w += -q.Pop()
 	}
-	// doesnt fit
-	if w > (*capital)[index] {
-		(*dp)[index][k] = knapsack(k, w, profits, capital, index-1, dp)
-	} else {
-		(*dp)[index][k] = max(
-			(*profits)[index]+knapsack(k-1, w, profits, capital, index-1, dp),
-			knapsack(k, w, profits, capital, index-1, dp),
-		)
-	}
-	return (*dp)[index][k]
+
+	return w
 }
